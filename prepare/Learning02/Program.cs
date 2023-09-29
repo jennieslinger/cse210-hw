3205e2669a9137b4480f22a2ce593d7105b1322f
jennieslinger <jennieslinger@gmail.com>
2023-09-29 09:31:42 -0700

    abstraction activity job and resume

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Uber";
+        job1._jobTitle = "CFO";
+        job1._startYear = 2010;
+        job1._endYear = 2012;
+
+        Job job2 = new Job();
+        job2._company = "The Ohio State University";
+        job2._jobTitle = "School Information Director";
+        job2._startYear = 2014;
+        job2._endYear = 2018;
+
+        Resume myResume = new Resume();
+        myResume._name = "John Doe";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
