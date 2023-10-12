eaa2467be15f228759e8c0cd898acbf3e9aa28cf
jennieslinger <jennieslinger@gmail.com>
2023-10-12 11:21:26 -0700

    Fraction

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,16 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction number1 = new Fraction();
+        Console.WriteLine(number1.GetFractionString());
+        Console.WriteLine(number1.GetDecimalValue());
+
+        Fraction number2 = new Fraction(1);
+        Console.WriteLine(number2.GetFractionString());
+        Console.WriteLine(number2.GetDecimalValue());
+
+        Fraction number3 = new Fraction(1,3);
+        Console.WriteLine(number3.GetFractionString());
+        Console.WriteLine(number3.GetDecimalValue());
     }
 }
